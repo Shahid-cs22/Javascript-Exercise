e84dfe5befcf9a89a70d2b5c719143484cd28c19
Shahid-cs22 <mohamedshahid.cs22@stellamaryscoe.edu.in>
2025-05-02 16:04:17 +0530

    Day two of JavaScript

---
+++ FunParameter.js
@@ -0,0 +1,48 @@
+// first function
+
+function name() {
+    console.log("Shahid");
+}
+name();
+
+
+// second function
+function add(num1, num2) {
+    console.log(num1 + num2);
+}
+add(5, 10);
+
+// third function
+
+function namefun(str1, str2) {
+    console.log(str1, str2)
+}
+namefun("Mohamed", "Shahid")
+
+// fourth function
+
+function namefun1(str1, str2, str3) {
+    console.log(str1);
+    console.log(str2, str3);
+
+}
+namefun1("Tamilnadu", "Kanyakumari", 629501);
+
+
+// fifth function
+
+function addNumber(num1, num2) {
+    const result = num1 + num2;
+    return result;
+}
+console.log(addNumber(88, 92));
+
+// sixth function
+
+
+function sFunction(name,age){
+    console.log(`Enter Your Name: ${name}`);
+    console.log(`Enter Your Age: ${age}`);
+
+};
+sFunction("Mohamed Shahid",20);
