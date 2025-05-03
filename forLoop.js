7b29eb4c95b904471b03eea527285abdaa146675
Shahid-cs22 <mohamedshahid.cs22@stellamaryscoe.edu.in>
2025-05-03 10:04:11 +0530

    Day 4 of JavaScript

---
+++ forLoop.js
@@ -0,0 +1,41 @@
+// Day 4 of JavaScript
+
+// First loop
+
+for (let i = 0; i <= 10; i++) {
+    console.log(i);
+}
+console.log("______________________________");
+
+for (let j = 10; j >= 0; j--) {
+
+    console.log(j);
+
+}
+console.log("Happy New Year 2025");
+console.log("_________________________");
+
+
+
+
+
+
+// Second loop
+
+for (let i = 0; i < 10; i++) {
+    console.log("loop is", i);
+}
+console.log("loop is finished");
+console.log("_____________________");
+
+
+
+
+for (let i=0; i<=20; i++){
+    if(i==10){
+        break;
+    }
+    else{
+        console.log(i);
+    }
+}
