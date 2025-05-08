e40c6f1efc7b34d6d774517fffb34fe9f867aaf9
Shahid-cs22 <mohamedshahid.cs22@stellamaryscoe.edu.in>
2025-05-08 19:44:38 +0530

    Day 6 of JavaScript

---
+++ Array.js
@@ -0,0 +1,35 @@
+// Day 6 of javascript
+
+// ARRAY Example 1
+console.log("------------");
+
+let district = ["Kanyakumari", "Chennai", "Dindugal", "Thirunelveli"];
+
+console.log(district);
+
+
+console.log(district[0]);
+console.log(district[1]);
+console.log(district[2]);
+console.log(district[3]);
+
+
+console.log("---------------");
+
+let numdistrict = district.length
+
+console.log(numdistrict);
+
+// another example
+
+let selectColors=['red','blue', 'green', 'black', 'white'];
+
+selectColors [0]='yellow'
+console.log(selectColors);
+console.log(selectColors.length);
+
+// another example
+
+const shedule =['Wake up', 'Eat', 'College', 'Study'];
+
+console.log(shedule);
